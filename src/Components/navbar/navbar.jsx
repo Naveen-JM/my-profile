b48202a000b12e5befffb4d1607ce123b7a63cd3
Naveen Janardhanan Murugan <2000129982@hexaware.com>
2025-04-01 14:00:03 +0530

    modified-name

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded shadow-sm my-3">
       <div className="container-fluid">
         <div className="container"> {/* Added a container for spacing */}
-          <a className="navbar-brand" href="#">Naveen Murugan</a>
+          <a className="navbar-brand" href="#">Naveen Janardhanan Murugan</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
