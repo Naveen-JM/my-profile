98273c116a906ce5951b9ac2f5ef5a05a7429b77
Naveen J Murugan <57980884+Naveen-JM@users.noreply.github.com>
2026-08-27 11:31:42 +0530

    Add AWS skill and update proficiency levels
    Added AWS Cloud skill and updated proficiency levels for PHP and Git.

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -8,16 +8,18 @@ import drupalIcon from "../../assets/skills/drupal.png";
 import phpIcon from "../../assets/skills/php.png";
 import acquiaIcon from "../../assets/skills/acquia.png";
 import gitIcon from "../../assets/skills/git.png";
+import awsIcon from "../../assets/skills/AWS.png";
 
 function Skills() {
   const skills = [
     { name: "Web Development(HTML,CSS & JS)", icon: webIcon, level: 85 },
+    { name: "AWS Cloud", icon: awsIcon, level: 70 },
     { name: "React", icon: reactIcon, level: 70 },
     { name: "Python", icon: pythonIcon, level: 75 },
     { name: "Drupal", icon: drupalIcon, level: 90 },
-    { name: "PHP", icon: phpIcon, level: 65 },
+    { name: "PHP", icon: phpIcon, level: 75 },
     { name: "Acquia Cloud", icon: acquiaIcon, level: 90 },
-    { name: "Git", icon: gitIcon, level: 65 },
+    { name: "Git", icon: gitIcon, level: 75 },
   ];
 
   return (
